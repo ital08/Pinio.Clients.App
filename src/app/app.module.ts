c5a5dcfbacc0ee8f6e42df7851393a0d71c32053
Italo <italo.rivadeneyra.r@uni.pe>
2021-09-29 21:10:50 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./layout/header/header.component";
 import { FooterComponent } from "./layout/footer/footer.component";
-import { NavComponent } from "./layout/nav/nav.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ContentlayoutComponent } from "./layout/contentlayout/contentlayout.component";
 import { CoreModule } from "./core/core.module";
